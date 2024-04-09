44abf37cae365259c7af201ce469bdb9c022ce55
ducndt-dgg <ducyb782001@gmail.com>
2024-04-09 10:24:50 +0700

    feat: remove unnecessary footer

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -12,9 +12,6 @@ function MainLayout({ lng, children }) {
       <div className="min-w-full min-h-screen md:pt-20 pt-[60px] md:pl-[100px] text-black bg-bgPrimary dark:bg-bgDarkSecondary">
         <div className={`px-4 pb-10 }`}>{children}</div>
       </div>
-      <div className="fixed bottom-0 w-full text-center md:ml-[50px] bg-bgPrimary dark:bg-bgDarkSecondary dark:text-textDark text-black text-xs pb-1">
-        <p> Copyright © 2024 Rasset LLC. All rights reserved</p>
-      </div>
     </div>
   )
 }
