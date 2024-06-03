8ff54539c86a62056fa7a7b6f51b34457d540f1a
ducndt-dgg <ducyb782001@gmail.com>
2024-06-03 14:46:23 +0700

    chore: add react select package

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,9 +3,19 @@
 import { useTranslation } from "@/app/i18n/client"
 import React, { useState } from "react"
 import PopupTemplate from "./common/PopupTemplate"
+import Select from "react-select"
+import Link from "next/link"
+
+const options = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+]
 
 function HomePage({ lng }) {
   const [isShowPopupAddStudent, setIsShowPopupAddStudent] = useState(false)
+  const [selectedOption, setSelectedOption] = useState<any>()
+
   const { t } = useTranslation(lng, "common")
   return (
     <div>
@@ -25,6 +35,22 @@ function HomePage({ lng }) {
       >
         <div className="text-secondary dark:text-textDark">Popup here</div>
       </PopupTemplate>
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+        className="w-[600px]"
+        isMulti
+      />
+      Code sanbox compile:
+      <Link
+        href={
+          "https://codesandbox.io/p/sandbox/formik-2-with-react-select-2-and-3-example-73jj9zom96?file=%2Findex.js%3A138%2C39"
+        }
+        target="_blank"
+      >
+        react select with useFormik
+      </Link>
     </div>
   )
 }
